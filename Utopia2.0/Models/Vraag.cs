088bbbbbad189a13ce1daef639fd0172f8e27946
Mathias <r0618721@student.thomasmore.be>
2022-12-01 11:05:17 +0100

    data models

---
+++ Utopia2.0/Models/Vraag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Utopia2._0.Models
+{
+    public class Vraag
+    {
+        public int VraagID { get; set; }
+        public string TextueleVraag { get; set; }
+        public int GebouwID { get; set; }
+
+        public Gebouw Gebouw { get; set; }
+
+        public ICollection<MeerkeuzeAntwoord> MeerKeuzeAntwoorden { get; set; }
+        public ICollection<Antwoord> Antwoorden { get; set; }
+    }
+}
