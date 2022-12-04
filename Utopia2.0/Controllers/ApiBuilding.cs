f90f4f4b0b30b7c5e7ef93dd813f4eb4387b6122
Stijn De Preter <stijndepreter@live.be>
2022-12-04 13:20:11 +0100

    methodes toegevoegd

---
+++ Utopia2.0/Controllers/ApiBuilding.cs
@@ -0,0 +1,10 @@
+﻿namespace Utopia2._0.Controllers
+{
+    public class ApiBuilding
+    {
+        public int Id { get; set; }
+        public string GraduateProgram { get; set; }
+        public string Color { get; set; }
+
+    }
+}
