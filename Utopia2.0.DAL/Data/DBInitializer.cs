b3dde141e5c2eab05941333aee71f18b6e004cf8
Mathias <r0618721@student.thomasmore.be>
2022-12-08 21:09:48 +0100

    dbdata first try

--- Utopia2.0.DAL/Data/DBInitializer.cs
+++ Utopia2.0.DAL/Data/DBInitializer.cs
@@ -1,36 +1,106 @@
-﻿using Utopia2._0.Data;
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using Utopia2._0.Data;
 using Utopia2._0.Models;
 
 namespace Shop.DAL.Models
 {
     public class DBInitializer
     {
+
         public static void Initialize(UtopiaContext context)
         {
+            Random rd = new Random();
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+
             context.Database.EnsureCreated();
             //Add Stations
-            Station station1 = new Station { X = 5.5, Y = 7.8 };
-            Line line1 = new Line { Color = "blue", Faculty = "Informatiewetenschappen" };
-            context.Add(station1);
-            context.Add(line1);
-            context.SaveChanges();
 
-            Building building1 = new Building { GraduateProgram = "Toegapse informatica", LineId = 1, StationId = 1 };
-            context.Add(building1);
-            context.SaveChanges();
-            Person person1 = new Person { Username = "Mathias", RandomKey = "2578528" };
-            context.Add(person1);
-            context.SaveChanges();
-            Question question1 = new Question { TextualQuestion = "wat is blabla?", BuildingId = 1};
-            context.Add(question1);
-            context.SaveChanges();
-            MultipleChoiceAnswer multipleChoiceAnswer1 = new MultipleChoiceAnswer { TextualAnswer = "dit is het antwoord", Correct = true, QuestionId = 1 };
-            context.Add(multipleChoiceAnswer1);
-            context.SaveChanges();
-            Answer answer1 = new Answer { MultipleChoiceAnswerId = 1, PersonId = 1};
-            context.Add(answer1);
-            context.SaveChanges();
-            
+            if (!context.Stations.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    double XDouble = rd.NextDouble();
+                    double YDouble = rd.NextDouble();
+                    Station station = new Station { X = XDouble, Y = YDouble };
+                    context.Add(station);
+                }
+                context.SaveChanges();
+            }
+            if (!context.Lines.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    KnownColor randomColorName = names[rd.Next(i)];
+                    string randomColor = randomColorName.ToString();
+                    //Color randomColor = Color.FromKnownColor(randomColorName);
+
+                    Line line = new Line { Color = randomColor, Faculty = "dit is een faculty" + i };
+
+                    context.Add(line);
+                }
+                context.SaveChanges();
+            }
+            if (!context.Buildings.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Building building = new Building { GraduateProgram = "dit is een gebouw" + i, LineId = i, StationId = i };
+
+                    context.Add(building);
+                }
+                context.SaveChanges();
+            }
+            if (!context.Persons.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    int randomNumber = rd.Next(1000000, 9999999);
+
+                    Person person = new Person { Username = "Persoon" + i, RandomKey = randomNumber };
+
+                    context.Add(person);
+
+                }
+                context.SaveChanges();
+            }
+            if (!context.Questions.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Question question = new Question { TextualQuestion = "dit is een vraag" + i, BuildingId = i };
+
+                    context.Add(question);
+                }
+                context.SaveChanges();
+            }
+            if (!context.MultipleChoiceAnswers.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    for (int j = 1; j <= 4; j++)
+                    {
+                        var randomBool = rd.Next(2) == 1;
+                        MultipleChoiceAnswer multipleChoiceAnswer = new MultipleChoiceAnswer { TextualAnswer = "dit is een antwoord" + j, Correct = randomBool, QuestionId = i };
+                        context.Add(multipleChoiceAnswer);
+                    }
+                }
+                context.SaveChanges();
+            }
+            if (!context.Answers.Any())
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Answer answer = new Answer { MultipleChoiceAnswerId = i, PersonId = i };
+                    context.Add(answer);
+                }
+                context.SaveChanges();
+            }
+
+
+
+
             // Look for any products.
             /*if (context.Products.Any())
             {
