6d766c77a1131e36fb50235270dd3b8c6881bca4
Mathias <r0618721@student.thomasmore.be>
2022-12-19 12:24:55 +0100

    fix voor frontend

--- Utopia2.0/Controllers/ApiSationByLine.cs
+++ Utopia2.0/Controllers/ApiSationByLine.cs
@@ -2,13 +2,14 @@
 
 namespace Utopia2._0.Controllers
 {
-    public class ApiStation
+    public class ApiStationByLines
     {
 
         public int Id { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
+        public List<ApiCoordinates> Coordinates { get; set; }
+
+        public string Color { get; set; }
+
         public int NumberOfBuildings { get; set; }
-        public int firstBuildingId { get; set; }
     }
 }
