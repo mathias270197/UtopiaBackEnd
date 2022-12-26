d2140854b0aae0512a8105371c0ea2c1bd168d63
Stijn De Preter <stijndepreter@live.be>
2022-12-26 22:48:01 +0100

    random date toegevoegd aan de answers

--- Utopia2.0/Controllers/BiController.cs
+++ Utopia2.0/Controllers/BiController.cs
@@ -34,62 +34,11 @@ namespace Utopia2._0.Controllers
                     faculty = a.MultipleChoiceAnswer.Question.GraduateProgram.Faculty.Name,
                     userName = a.Person.Username,
                     userKey = a.Person.Userkey,
+                    date = a.Date
                 })
                 .ToListAsync();
 
             return powerBiInput;
         }
-
-
-
-
-
-
-
-
-
-        //[HttpGet("GetAnswers")]
-        //public async Task<ActionResult<IEnumerable<Answer>>> GetAnswers()
-        //{
-        //    return await _context.Answers.ToListAsync();
-        //}
-
-        //[HttpGet("GetBuildings")]
-        //public async Task<ActionResult<IEnumerable<Building>>> GetBuildings()
-        //{
-        //    return await _context.Buildings.ToListAsync();
-        //}
-
-        //[HttpGet("GetLines")]
-        //public async Task<ActionResult<IEnumerable<Line>>> GetLines()
-        //{
-        //    return await _context.Lines.ToListAsync();
-        //}
-
-        //[HttpGet("GetMultipleChoiceAnswers")]
-        //public async Task<ActionResult<IEnumerable<MultipleChoiceAnswer>>> GetMultipleChoiceAnswer()
-        //{
-        //    return await _context.MultipleChoiceAnswers.ToListAsync();
-        //}
-
-        //[HttpGet("GetPersons")]
-        //public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
-        //{
-        //    return await _context.Persons.ToListAsync();
-        //}
-
-        //[HttpGet("GetQuestions")]
-        //public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
-        //{
-        //    return await _context.Questions.ToListAsync();
-        //}
-
-        //[HttpGet("GetStations")]
-        //public async Task<ActionResult<IEnumerable<Station>>> GetStations()
-        //{
-        //    return await _context.Stations.ToListAsync();
-        //}
-
-
     }
 }
